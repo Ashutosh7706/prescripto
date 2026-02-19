44dabc68b62157666232e63e6473ee7fde08c857
goCart <www.ashuty1234@gmail.com>
2026-02-19 20:47:12 +0530

    messafe

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
-import TopDoctors from '../components/topDoctors'
-import Banner from '../components/banner'
+import TopDoctors from '../components/TopDoctors'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
